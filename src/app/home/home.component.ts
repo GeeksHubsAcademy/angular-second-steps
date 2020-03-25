198e049a1e90e491edb6d210debac5b57f1f9829
DavidPinilla92 <d.pinilla@geekshubs.com>
2020-03-25 13:30:04 +0100

    added products

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public products;
+  // public products:Array<object>;
+  constructor(public httpClient: HttpClient) {//inyección de dependencias, inyectamos httpClient como dependecia del HomeComponent
+  }
+  //  public httpClient;
+  //  constructor(HttpClient) {
+  //   this.httpClient = new HttpClient();
+  //  }
 
   ngOnInit(): void {
+    this.getAllProducts();
   }
-
+  getAllProducts(){
+    this.httpClient.get('http://localhost:3000/products')
+      .subscribe(
+        res => {
+          this.products = res;
+        },
+        error => console.error(error)
+      );
+  }
 }
